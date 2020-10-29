49ce7180d39f4cdc55d2836825599e21b9ceea7c
MrChuckNovice <teedji77@hotmail.fr>
2020-10-29 09:30:33 +0100

    test mysql

---
+++ includes/inc_files.php
@@ -0,0 +1,4 @@
+<?php
+if(__FILE__==$_SERVER['SCRIT_FILENAME'])exit('No direct access allowed.');
+require_once("C:\xampp\htdocs\includes\configuration\config.php");
+?>
--- index.php
+++ index.php
@@ -1,26 +1,19 @@
 <?php
-if (empty($_POST))
-$villechoisie=($_POST['ville']);
+require_once("C:\xampp\htdocs\includes\inc_files.php");
+$pdo= new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_NAME . ';charset=itf8', DB_USER, DB_PASS, array(
+    PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8 COLLATE utf8_general_ci',
+    PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));// On crée une instance de la classe PDO qui par défaut nous connecte à la base de données.
 
-switch ($villechoisie) {
-    case "paris":
-        echo "Paris est la capitale de la France";
-    break;
-    case "moscou":
-        echo "Moscou est la capitale de la Fédération de Russie";
-    break;
-    case "londre":
-        echo "Londre est la capitale de la Grande-Bretagne";
-    break;
-}
-?>
-/
-<form action="index.php" method="post">
-          <label for="ville">choisit une ville:</label>
-          <select id="ville" name="ville">
-               <option value="paris">paris</option>
-               <option value="moscou">moscou</option>
-               <option value="londre">londre</option>
-            </select>
-          <input type="submit">
-    </form>
+    //********************************/
+    $query = $pdo->query('SELECT*FROM tb_client');
+    
+    if ($query === false) {
+        var_dump($pdo->errorInfo());
+        die('Erreur sql');
+    }
+
+    $data = $query->fetchAll( PDO::FETCH_ASSOC);
+    var_dump($data);
+
+    ?>
+
